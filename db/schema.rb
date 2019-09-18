444ac265120fd2045f858733a67d8608c1fdb4a2
CarlH2019 <hofstromcarl@gmail.com>
2019-09-18 13:56:44 +0000

    after resolving schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2019_09_18_030438) do
+
+ActiveRecord::Schema.define(version: 2019_09_18_023054) do
+
 
 
   # These are extensions that must be enabled in order to support this database
