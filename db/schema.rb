51a8459f905de83ab549189b0938ab2a2765fb69
Travis Watson <watsonjamest@gmail.com>
2019-09-19 07:31:25 -0500

    Fix gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_09_17_182033) do
-
-
-
-ActiveRecord::Schema.define(version: 2019_09_18_023054) do
-
-
-
-ActiveRecord::Schema.define(version: 2019_09_17_221327) do
-
-ActiveRecord::Schema.define(version: 2019_09_18_023054) do
-
+ActiveRecord::Schema.define(version: 2019_09_18_030438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -60,8 +47,10 @@ ActiveRecord::Schema.define(version: 2019_09_18_023054) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -79,21 +68,5 @@ ActiveRecord::Schema.define(version: 2019_09_18_023054) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid"], name: "index_users_on_uid"
   end
-
-end
-
-create_table "users", force: :cascade do |t|
-  t.string "email", default: "", null: false
-  t.string "encrypted_password", default: "", null: false
-  t.string "reset_password_token"
-  t.datetime "reset_password_sent_at"
-  t.datetime "remember_created_at"
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-  t.index ["email"], name: "index_users_on_email", unique: true
-  t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  t.integer "user_id"
-  t.index ["user_id"], name: "index_places_on_user_id"
-end
 
 end
