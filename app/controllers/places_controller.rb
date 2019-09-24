849adaa136a214313a6714bf05a8d3ad3a9149c4
Travis Watson <watsonjamest@gmail.com>
2019-09-24 14:08:56 -0500

    Add option to edit and destroy places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -36,12 +36,12 @@ class PlacesController < ApplicationController
     def update
       @place = Place.find(params[:id])
       if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+        return render plain: 'Not Allowed', status: :forbidden
+      end
   
       @place.update_attributes(place_params)
       if @place.valid?
-      redirect_to root_path
+        redirect_to root_path
       else
         render :edit, status: :unprocessable_entity
       end
@@ -50,8 +50,8 @@ class PlacesController < ApplicationController
     def destroy
       @place = Place.find(params[:id])
       if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+        return render plain: 'Not Allowed', status: :forbidden
+      end
   
       @place.destroy
       redirect_to root_path
