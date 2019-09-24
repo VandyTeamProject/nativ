bcfb764866ed7c11d7fbd001442425b0bd1fbd4e
Lauren Raczkowsk <lracz117@gmail.com>
2019-09-24 01:15:16 +0000

    toggle bar showing up

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_023054) do
+ActiveRecord::Schema.define(version: 2019_09_17_221327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "message"
-    t.string "rating"
-    t.integer "place_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
