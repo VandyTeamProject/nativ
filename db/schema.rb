6572f0a4b07b23a2d2c56380babad9dbb7551140
Christopher Oye <christopheroye@gmail.com>
2019-09-24 23:06:15 +0000

    add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_221327) do
+ActiveRecord::Schema.define(version: 2019_09_24_222718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -23,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_221327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
