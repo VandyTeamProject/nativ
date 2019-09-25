5dda15f1fe73f927a039262d055f9dacd64c0300
CarlH2019 <hofstromcarl@gmail.com>
2019-09-25 15:29:51 +0000

    1 test fails, redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create]
 
   def create
-    @place = Place.find(params[:place_id])
+    @place = Place.find_by_id(params[:place_id])
+    return render_not_found if @gram.blank?
     @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 
   def comment_params
     params.require(:comment).permit(:message, :rating)
--- spec/controllers/comment_tests_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CommentTestsController, type: :controller do
-
-end
