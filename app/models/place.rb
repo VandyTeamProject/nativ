ad0acb55dc9211f77756681264cbabf776a074be
Travis Watson <watsonjamest@gmail.com>
2019-10-01 16:19:16 -0500

    Add map to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,8 @@ class Place < ApplicationRecord
   has_one :city
   has_many :comments
   has_many :images
+  geocoded_by :address
+  after_validation :geocode
 
   
   validates :name, presence: true
