7c2e5e0d2b5276fa92c86330b1cdc5c3ca83a0c7
Lauren Raczkowsk <lracz117@gmail.com>
2019-10-01 23:57:00 +0000

    updated image uploader to store photos locally

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_194408) do
+ActiveRecord::Schema.define(version: 2019_10_01_202957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,22 @@ ActiveRecord::Schema.define(version: 2019_10_01_194408) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
+  end
+
+  create_table "saves", force: :cascade do |t|
+    t.string "saveable_type"
+    t.bigint "saveable_id"
+    t.string "saver_type"
+    t.bigint "saver_id"
+    t.boolean "save_flag"
+    t.string "save_scope"
+    t.integer "save_weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["saveable_id", "saveable_type", "save_scope"], name: "index_saves_on_saveable_id_and_saveable_type_and_save_scope"
+    t.index ["saveable_type", "saveable_id"], name: "index_saves_on_saveable_type_and_saveable_id"
+    t.index ["saver_id", "saver_type", "save_scope"], name: "index_saves_on_saver_id_and_saver_type_and_save_scope"
+    t.index ["saver_type", "saver_id"], name: "index_saves_on_saver_type_and_saver_id"
   end
 
   create_table "users", force: :cascade do |t|
