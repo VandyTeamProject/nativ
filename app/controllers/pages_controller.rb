2380f43fa98112ce32f4efa3ee8e4a926632c20a
Christopher Oye <christopheroye@gmail.com>
2019-10-02 03:29:40 +0000

    updated search field box

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+    def search  
+        if params[:search].blank?  
+          redirect_to(root_path, alert: "Empty field!") and return  
+        else    
+            @parameter = params[:search].downcase  
+            @results = Store.all.where("lower(name) LIKE :search", search: @parameter)
+        end  
+    end
+end
+
