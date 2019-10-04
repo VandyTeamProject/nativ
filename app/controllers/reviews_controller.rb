130adc4ea4357ac2a361fde0de69341eb86a06b0
CarlH2019 <hofstromcarl@gmail.com>
2019-10-04 15:14:05 +0000

    not connecting to place page

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,17 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def create
+    @place = Place.find(params[:place_id])
+    @place.reviews.create(review_params.merge(user: current_user))
+    redirect_to place_path(@place)
+  end
+
+    private
+
+  def review_params
+    params.require(:review).permit(:rating)
+  end
+
+end
--- db/migrate/20191002223839_create_reviews.rb
+++ db/migrate/20191002223839_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.text :message
+      t.string :rating
       t.integer :user_id
       t.integer :place_id
       t.timestamps
