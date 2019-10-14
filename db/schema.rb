cb4894d9ebc11bf03f72963b11fadabe8fefce53
Lauren Raczkowsk <lracz117@gmail.com>
2019-10-14 23:23:31 +0000

    run heroku db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -54,12 +54,16 @@ ActiveRecord::Schema.define(version: 2019_10_10_204208) do
     t.bigint "phone_number"
     t.text "description"
     t.string "images"
+    t.boolean "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "image"
     t.float "latitude"
     t.float "longitude"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
     t.string "website"
     t.string "hours"
     t.string "category"
@@ -75,6 +79,12 @@ ActiveRecord::Schema.define(version: 2019_10_10_204208) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_reviews_on_place_id"
     t.index ["user_id", "place_id"], name: "index_reviews_on_user_id_and_place_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
