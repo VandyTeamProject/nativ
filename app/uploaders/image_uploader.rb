b7ce7fa0cb3f248268325aeae20fa8fc09ab656d
Travis Watson <watsonjamest@gmail.com>
2019-10-23 12:27:07 -0500

    Configure image storage for AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -39,6 +39,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
+  storage :fog
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
